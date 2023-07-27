4db051085532144ec5fb9af575d4887d5eaaf00e
jamiesoconnell <jamieoconnell6@gmail.com>
2023-07-27 11:30:50 -0400

    3

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 const { MongoClient } = require('mongodb');
 
 const app = express();
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 3000;
 const connectionString = process.env.MONGODB_CONNECTION_STRING;
 
 async function connectToDatabase() {
