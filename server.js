b8de9d2deebfc9f3021fa5838b47bb8d8b484e9e
jamiesoconnell <jamieoconnell6@gmail.com>
2023-07-27 10:36:15 -0400

    async add

--- server.js
+++ server.js
@@ -12,70 +12,72 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
 app.use(bodyParser.json());
 
-MongoClient.connect(connectionString, { useUnifiedTopology: true })
-  .then(client => {
+async function connectToDatabase() {
+  try {
+    const client = await MongoClient.connect(connectionString, { useUnifiedTopology: true });
     console.log('Connected to Database');
     const db = client.db('Robin-Williams-Quotes');
     const quotesCollection = db.collection('quotes');
+
+    app.get('/', async (req, res) => {
+      try {
+        const results = await quotesCollection.find().toArray();
+        console.log(results);
+        res.render('index.ejs', { quotes: results });
+      } catch (error) {
+        console.error(error);
+      }
+    });
+
+    app.post('/quotes', async (req, res) => {
+      try {
+        const result = await quotesCollection.insertOne(req.body);
+        res.redirect('/');
+      } catch (error) {
+        console.error(error);
+      }
+      console.log(req.body);
+    });
+
+    app.put('/quotes', async (req, res) => {
+      try {
+        const result = await quotesCollection.findOneAndUpdate(
+          { name: 'Robin Williams' },
+          {
+            $set: {
+              name: req.body.name,
+              quote: req.body.quote,
+            },
+          },
+          {
+            upsert: true,
+          }
+        );
+        res.json('Success');
+      } catch (error) {
+        console.error(error);
+      }
+    });
+
+    app.delete('/quotes', async (req, res) => {
+      try {
+        const result = await quotesCollection.deleteOne({ name: req.body.name });
+        if (result.deletedCount === 0) {
+          return res.json('No quote to delete');
+        }
+        res.json('Deleted quote');
+      } catch (error) {
+        console.error(error);
+      }
+    });
 
     app.listen(PORT, () => {
       console.log(`Listening on port ${PORT}`);
     });
 
-    app.get('/', (req, res) => {
-      quotesCollection.find().toArray()
-        .then(results => {
-          console.log(results);
-          res.render('index.ejs', { quotes: results });
-        })
-        .catch(error => console.error(error));
-        
-    });
+  } catch (error) {
+    console.error(error);
+  }
+}
 
-    app.post('/quotes', (req, res) => {
-      quotesCollection
-        .insertOne(req.body)
-        .then(result => {
-          res.redirect('/');
-        })
-        .catch(error => console.error(error));
-      console.log(req.body);
-    });
-
-    app.put('/quotes', (req, res) => {
-      quotesCollection.findOneAndUpdate(
-        { name: 'Robin Williams' },
-        {
-          $set: {
-            name: req.body.name,
-            quote: req.body.quote
-          }
-        },
-        {
-          upsert: true
-        }
-      )
-        .then(result => res.json('Success'))
-        .catch(error => console.error(error));
-    });
-
-    app.delete('/quotes', (req, res) => {
-      quotesCollection.deleteOne(
-        { name: req.body.name }
-      )
-        .then(result => {
-          if (result.deletedCount === 0) {
-            return res.json('No quote to delete');
-          }
-          res.json('Deleted quote');
-        })
-        .catch(error => console.error(error));
-    });
-    
- 
-    // app.listen(PORT, () => {
-    //   console.log(`Listening on port ${PORT}`);
-    // });
-
-  })
-  .catch(error => console.error(error));
+connectToDatabase();
